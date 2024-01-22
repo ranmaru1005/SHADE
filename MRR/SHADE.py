b42bcecbd43ae739ced7527dd5c06e63f716bf24
ranmaru1005 <145675992+ranmaru1005@users.noreply.github.com>
2024-01-23 07:30:49 +0900

    Add files via upload

--- MRR/SHADE.py
+++ MRR/SHADE.py
@@ -56,6 +56,10 @@ def SHADE(func, bounds, params, pop_size=15, max_iter=500, H =50,  ftol=10**-4, 
             obj_list[j], populations[j], S_F, S_CR, delta_fk, Archive = selection(func, params, j, obj_list_G, populations_G, all_trial[j], all_Fi[j], all_CRi[j], S_F, S_CR, delta_fk, Archive, Archivetimes)
         
         if S_F.size !=0 and S_CR.size !=0:
+            print("いまからメモリを計算する")
+            print("S_F = ",S_F)
+            print("S_CR = ",S_CR)
+            print("delta_fk = ",delta_fk)
             MF_para_H[k] = ( sum( ( delta_fk * (S_F ** S_F) ) / sum(delta_fk) ) ) / ( sum( ( delta_fk * S_F ) / sum(delta_fk) ) )
             MCR_para_H[k] = sum( ( delta_fk * S_CR ) / sum(delta_fk) )
             k = k + 1
