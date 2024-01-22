a438d50b73901a9a0dcbdc0b66da610c1371b4ec
ranmaru1005 <145675992+ranmaru1005@users.noreply.github.com>
2024-01-23 04:35:38 +0900

    Add files via upload

--- MRR/SHADE.py
+++ MRR/SHADE.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 import math
+import time
 from multiprocessing import Pool	#宮崎で追加
 
 
@@ -23,6 +24,7 @@ def SHADE(func, bounds, params, pop_size=15, max_iter=500, H =50,  ftol=10**-8, 
     k = 0       #メモリに更新を行った回数を数えるための変数
     Archive = [ [0.0] * xdim ] *  pop_size    #外部アーカイブ。解候補の数だけ存在する。
     Archivetimes = 0        #外部アーカイブに更新が入った回数を記録する。アーカイブがあふれるまでカウントを続けさせる。
+    time_sta = time.perf_counter()      #時間計測開始
 
     for i in range(max_iter):
         
@@ -48,21 +50,8 @@ def SHADE(func, bounds, params, pop_size=15, max_iter=500, H =50,  ftol=10**-8, 
             all_Fi[i] = tmp[i][1]
             all_CRi[i] = tmp[i][2]
 
-        
-
-        
-
                 
         for j in range(pop_size):
-            """
-            mutated, Fi = mutation(MF_para_H[r], bounds, j, pop_size, obj_list_G, populations_G, P_i_int, Archive, rng)
-            print("Fi = ",Fi)
-
-            trial, CRi = crossover(mutated, populations_G[j], xdim, MCR_para_H[r], rng)
-            print("CRi = ",CRi)
-            print("trial = ",trial)
-
-            """
             
             obj_list[j], populations[j], S_F, S_CR, delta_fk, Archive = selection(func, params, j, obj_list, populations, all_trial[j], all_Fi[j], all_CRi[j], S_F, S_CR, delta_fk, Archive, Archivetimes)
         
@@ -88,6 +77,9 @@ def SHADE(func, bounds, params, pop_size=15, max_iter=500, H =50,  ftol=10**-8, 
         if callback is not None:
             callback(i, best_x, best_obj, populations)
 
+        time_end = time.perf_counter()
+        tim = time_end - time_sta
+        print("現在の経過時間は",tim)
     
     return best_x, best_obj     #best_x➡最適化が終わったK, best_obj➡最適化が終わったE
     
@@ -133,47 +125,6 @@ def mut_cross(MF_para_H, MCR_para_H, bounds, j, pop_size, obj_list_G, population
 def wrapper_mut_cross(args):
     return mut_cross(*args)
 
-
-def mutation(MF_para_H, bounds, j, pop_size, obj_list_G, populations_G, P_i_int, Archive, rng):
-    select_populations = Archive + populations_G
-    Fi = -1.0
-    while Fi <= 0.0:
-        Fi = rng.normal(MF_para_H,0.316227766)
-        if Fi > 1.0:
-            Fi = 1.0
-    A = np.array(obj_list_G)
-    A_sort_index = np.argsort(A)[::-1]        #ここ二行でobj_list_Gのソートを行っている。評価の良い順に並べ、そのインデックスがリストになっている。
-    xpbest_group = [populations_G[A_sort_index[i]] for i in range(P_i_int)]      #G世代の解候補の中から、評価が高いものをP_i_intの数だけ選んだ集合を作る。
-    xpbest = random.choice(xpbest_group)        #G世代の解候補の中から上位N×P番目までの候補から一つを選んだ。
-    indexes = [i for i in range(pop_size) if i != j]        #jは現在選んでいる解。それ以外の番号を指定しているインデックスを作成
-    a = populations_G[rng.choice(indexes, 1, replace = False)]        #現在選んでいる解以外から1つを選ぶ。
-    b = random.choice(select_populations)       #アーカイブも含めて解候補の中から解を一つ選ぶ。➡「要改変」アーカイブは問題ないが、G世代の解候補から選ぶ際に、jを除いていない。これにより注目しているものと同じ要素を選ぶ可能性がある。
-    while np.all(b == 0.0):           #bが0のときは一生新たに選択をする。bがゼロとなるのは埋まっていないアーカイブを選択した場合である。
-        b = random.choice(select_populations)
-    mutated = populations_G[j] + Fi * (xpbest - populations_G[j]) + Fi * (a - b)       #突然変異を表す式。「要改変」➡現在はカレントトゥベスト➡最終的にはカレントトゥピーベストにする
-    mutated = np.clip(mutated, bounds[:, 0], bounds[:, 1])      #変異によって生まれたベクトルが異常な場合、値を範囲内に収める。
-
-    return mutated, Fi      #mutatedは新たな解候補として生み出されたものであり、populationsと同様に最適化を行う結合率分のリストである。
-
-
-def crossover(mutated , target, dims, MCR_para_H, rng):
-    trial = np.zeros(dims)
-    CRi = rng.normal(MCR_para_H,0.316227766)
-    if CRi > 1:
-        CRi = 1
-    elif CRi < 0:
-        CRi = 0
-    p = rng.random(dims)        #0~1の値をランダムにxdimの数だけ生成する
-    p[rng.choice([i for i in np.arange(len(p))], 1)] = 0      #pの中で一つだけ確定で0にする。こうすることによってcrよりpが小さくなるのが一つ以上できるので、確定で一つはmutatedになる。
-    
-    for i in range(dims):        #crよりpが小さい場合はmutated,そうでなければ変更しないようにする。
-        if p[i] <= CRi:
-            trial[i] = mutated[0][i]
-        else:
-            trial[i] = target[i]
-
-    return trial, CRi      #trialは新たな解候補である。mutatedの一部要素と、従来の注目していた点の要素を持つ。
-    
 
 def selection(func, params, j, obj_list, populations, trial, Fi, CRi, S_F, S_CR, delta_fk, Archive, Archivetimes):
     obj_trial = func(trial, params)     #交叉によって生成された解候補(pop_size分だけある)の評価値を計算する
