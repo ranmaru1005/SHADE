68679f5246fc31c17a3f2ef5f7cd569d6b8fda3a
ranmaru1005 <145675992+ranmaru1005@users.noreply.github.com>
2024-02-15 00:30:32 +0900

    Update DE.py

--- MRR/model/DE.py
+++ MRR/model/DE.py
@@ -65,7 +65,7 @@ class OptimizeKParams:
     r_max: float
     weight: list[float]
 
-"""
+
     #もともとのプログラムはこれ。
 def optimize_K(
     eta: float,
@@ -91,12 +91,12 @@ def optimize_K(
     K: npt.NDArray[np.float_] = result.x
 
     return K, E
-"""
 
 
 
 
 
+"""  
 def optimize_K(             #通常のSHADE用
     eta: float,
     number_of_rings: int,
@@ -120,7 +120,7 @@ def optimize_K(             #通常のSHADE用
     K: npt.NDArray[np.float_] = result[0]
 
     return K, E
-
+"""  
 
 """  
 
