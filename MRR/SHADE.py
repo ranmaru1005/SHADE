7fd229b9f637e176ad40c20194c6cff01c4364e9
ranmaru1005 <145675992+ranmaru1005@users.noreply.github.com>
2024-06-10 15:13:37 +0900

    Update SHADE.py

--- MRR/SHADE.py
+++ MRR/SHADE.py
@@ -122,9 +122,10 @@ def mut_cross(MF_para_H, MCR_para_H, bounds, j, pop_size, obj_list_G, population
 
     #current-to-pbest　これは自分が設計したもの
 
-    
-    a = populations_G[rng.choice(indexes, 1, replace = False)]        #現在選んでいる解以外から1つを選ぶ。
-    b = random.choice(select_populations)       #アーカイブも含めて解候補の中から解を一つ選ぶ。➡「要改変」アーカイブは問題ないが、G世代の解候補から選ぶ際に、jを除いていない。これにより注目しているものと同じ要素を選ぶ可能性がある。
+    a = random.choice(populations_G)              #すべての解候補から一つ解を選ぶ。 試しに解の範囲を変更してみる
+    b = random.choice(populations_G)              #すべての解候補から一つ解を選ぶ。 同上
+    #a = populations_G[rng.choice(indexes, 1, replace = False)]        #現在選んでいる解以外から1つを選ぶ。
+    #b = random.choice(select_populations)       #アーカイブも含めて解候補の中から解を一つ選ぶ。➡「要改変」アーカイブは問題ないが、G世代の解候補から選ぶ際に、jを除いていない。これにより注目しているものと同じ要素を選ぶ可能性がある。
     while np.all(b == 0.0):           #bが0のときは一生新たに選択をする。bがゼロとなるのは埋まっていないアーカイブを選択した場合である。
         b = random.choice(select_populations)
     mutated = populations_G[j] + Fi * (xpbest - populations_G[j]) + Fi * (a - b)       #突然変異を表す式。「要改変」➡現在はカレントトゥベスト➡最終的にはカレントトゥピーベストにする
