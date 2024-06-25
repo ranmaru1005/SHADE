87332a2e83395c7e19b0060f6e82ad31f0a398e9
ranmaru1005 <145675992+ranmaru1005@users.noreply.github.com>
2024-06-26 04:07:44 +0900

    Add files via upload

--- hyouka_saitekika.py
+++ hyouka_saitekika.py
@@ -1,11 +1,32 @@
 import math
 import numpy as np
+import numpy.typing as npt
 
 from scipy.optimize import differential_evolution
 from MRR.SHADE import SHADE
 from MRR.benchmark_function import BenchmarkFunction as BF
 
 bf = BF()
+
+class OptimizeKParams:
+    L: npt.NDArray[np.float_]
+    n_g: float
+    n_eff: float
+    eta: float
+    alpha: float
+    center_wavelength: float
+    length_of_3db_band: float
+    FSR: np.float_
+    max_crosstalk: float
+    H_p: float
+    H_s: float
+    H_i: float
+    r_max: float
+    weight: list[float]
+
+
+
+
 
 #Sphere
 a = 5.12
@@ -16,6 +37,9 @@ max_iter = 6000
 H = 50
 tol = 0.01
 rng = np.random.Generator
+
+
+
 
 print( SHADE(bf.rastrigin, bounds, params, pop_size, max_iter, H, tol, callback = None, rng = None) )
 
