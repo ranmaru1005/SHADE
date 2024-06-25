bdaead1213e8d260483101394aa587700bf8f8a4
ranmaru1005 <145675992+ranmaru1005@users.noreply.github.com>
2024-06-26 03:37:10 +0900

    Add files via upload

--- hyouka_saitekika.py
+++ hyouka_saitekika.py
@@ -12,8 +12,22 @@ a = 5.12
 bounds = np.array((-a,a))
 params = 0
 pop_size = 10
-max_iter = 3000
+max_iter = 6000
 H = 50
 tol = 0.01
+rng = np.random.Generator
 
 print( SHADE(bf.rastrigin, bounds, params, pop_size, max_iter, H, tol, callback = None, rng = None) )
+
+result = differential_evolution(bf.rastrigin, 
+                            bounds, 
+                            args=(params,), 
+                            strategy="currenttobest1bin", 
+                            workers=-1, 
+                            updating="deferred", 
+                            popsize=15,
+                            maxiter=500,
+                            seed=rng)
+
+print("E = ",result.fun)
+print("X(解) = ",result.x)
