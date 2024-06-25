3dcd09c678e44c5ea0861c8f498b1145592652b2
ranmaru1005 <145675992+ranmaru1005@users.noreply.github.com>
2024-06-26 05:52:11 +0900

    Update hyouka_saitekika.py

--- hyouka_saitekika.py
+++ hyouka_saitekika.py
@@ -9,23 +9,6 @@ from MRR.benchmark_function import BenchmarkFunction as BF
 
 bf = BF()
 
-class OptimizeKParams:
-    L: npt.NDArray[np.float_]
-    n_g: float
-    n_eff: float
-    eta: float
-    alpha: float
-    center_wavelength: float
-    length_of_3db_band: float
-    FSR: np.float_
-    max_crosstalk: float
-    H_p: float
-    H_s: float
-    H_i: float
-    r_max: float
-    weight: list[float]
-
-
 
 
 
@@ -33,9 +16,9 @@ class OptimizeKParams:
 a = 5.12
 number_of_x = 2 #解の個数(次元の数ともいえる)
 #bounds = np.array([[-a, a] for _ in range(number_of_x)])
-A_bounds = np.array([(-a, a) for _ in range(number_of_x)])
-#bounds = np.array([-a, a])
-bounds = A_bounds.reshape(-1,1)
+#A_bounds = np.array([(-a, a) for _ in range(number_of_x)])
+bounds = np.array([-a, a],[-a, a])
+#bounds = A_bounds.reshape(-1,1)
 params = 0
 pop_size = 10
 max_iter = 6000
