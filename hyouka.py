d9dc78809494cfdfc5db1985580d3faab352e5f1
ranmaru1005 <145675992+ranmaru1005@users.noreply.github.com>
2024-06-26 07:10:00 +0900

    Update hyouka.py

--- hyouka.py
+++ hyouka.py
@@ -112,7 +112,7 @@ print("len(bounds.shape) = ",len(bounds.shape))
 
 #print( SHADE(bf.rastrigin, bounds, params, pop_size, max_iter, H, tol, callback = None, rng = None) )
 
-result = differential_evolution(ackley, 
+result = differential_evolution(schaffer, 
                             bounds, 
                             strategy="currenttobest1bin",
                             #disp = True,
