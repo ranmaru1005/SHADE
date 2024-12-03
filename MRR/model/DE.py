97540da4dc4acbaf4471a8329b7ba9622922f8e4
ranmaru1005 <145675992+ranmaru1005@users.noreply.github.com>
2024-12-04 02:43:13 +0900

    Update DE.py

--- MRR/model/DE.py
+++ MRR/model/DE.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 import numpy as np
 import numpy.typing as npt
 #from scipy.optimize import differential_evolution    #Scipyを使うときはこちらのコメントアウトを削除
-from MRR.DE_myself import differential_evolution
+#from MRR.DE_myself import differential_evolution
 from MRR.SHADE_old import SHADE        #20231219 に追加。　未完成のSHADEの導入
 from MRR.SHADE_myself import shade
 
