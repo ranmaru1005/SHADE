50e84e0e730321230d6257ea8fdc39c20685e59e
ranmaru1005 <145675992+ranmaru1005@users.noreply.github.com>
2025-01-16 05:32:45 +0900

    Update check_error_after.py

--- check_error_after.py
+++ check_error_after.py
@@ -129,4 +129,4 @@ params = OptimizeKParams(
 )
 
 # 誤差を加えた評価値の計算
-evaluate_with_error(K=K_theoretical, params=params, error_value=-0.005)
+evaluate_with_error(K=K_theoretical, params=params, error_value=0.005)
