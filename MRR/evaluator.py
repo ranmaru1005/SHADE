51506171bdf0e96984a380e33f5f40b21ec8c78f
ranmaru1005 <145675992+ranmaru1005@users.noreply.github.com>
2025-07-29 23:01:30 +0900

    Update evaluator.py

--- MRR/evaluator.py
+++ MRR/evaluator.py
@@ -231,6 +231,8 @@ def _evaluate_ripple(
         E = 1 - (std_ripple + range_ripple) / (r_max1 * r_max)
 
     return (np.float_(E), True)
+
+
 """
 def _evaluate_cross_talk(
     y: npt.NDArray[np.float_], max_crosstalk: float, pass_band_start: int, pass_band_end: int
@@ -247,6 +249,8 @@ def _evaluate_cross_talk(
         return (1 / (1 + penalty), False)
     return (np.float_(1), True)
 """
+
+
 def _evaluate_cross_talk(  y: npt.NDArray[np.float_], max_crosstalk: float, pass_band_start: int, pass_band_end: int
 ) -> tuple[np.float_, bool]:
     overall_peak = np.max(y)
