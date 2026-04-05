6e0c1f676430b90ec63d304b8e5d2c4fcf7cef6f
ranmaru1005 <145675992+ranmaru1005@users.noreply.github.com>
2026-04-06 00:08:17 +0900

    Update DE.py

--- MRR/model/DE.py
+++ MRR/model/DE.py
@@ -213,8 +213,7 @@ def optimize_K(
 """
 
 
-"""
-    #もともとのプログラムはこれ。
+
 def optimize_K(
     eta: float,
     number_of_rings: int,
@@ -241,7 +240,7 @@ def optimize_K(
     K: npt.NDArray[np.float_] = result.x
 
     return K, E
-"""
+
 
 
 
@@ -281,7 +280,7 @@ def optimize_K_with_perturbation(
 
 
 
-
+"""
 def optimize_K(             #通常のSHADE用
     eta: float,
     number_of_rings: int,
@@ -305,7 +304,7 @@ def optimize_K(             #通常のSHADE用
     K: npt.NDArray[np.float_] = result[0]
 
     return K, E
-
+"""
 
 
 
